3600d03071614e26445f5aab83e728cb55874df7
kmodi <konark.modi@holidaycheck.com>
2021-06-23 17:37:29 +0200

    fix paths

--- server/routes/home.js
+++ server/routes/home.js
@@ -3,9 +3,8 @@
 module.exports = app => {
 	// Redirect to the docs page
 	app.get("/", (request, response) => {
-		response.set({
-			"cache-control": "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800"
-		});
-		response.redirect(301, `${request.basePath}v3/`);
+		response.status(200);
+		response.set("Cache-Control", "max-age=0, must-revalidate, no-cache, no-store, private");
+		response.json({});
 	});
 };
